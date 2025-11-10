8422761e1f37d48c86f809e6e28edd662052162b
Novice-6 <419664199@qq.com>
2025-11-10 17:24:35 +0800

    Update GeneralPractitioner.java(comments)
    Add more detailed comments

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -6,19 +6,28 @@ public class GeneralPractitioner extends HealthProfessional {
     // Additional instance variable specific to a General Practitioner
     private String clinicLocation;
 
-    // Default constructor
+    /**
+     * Default constructor
+     * Calls the superclass default constructor to initialize the clinic location as "unallocated".
+     */
     public GeneralPractitioner() {
         super();
         this.clinicLocation = "Not assigned";
     }
 
-    // Constructor initializing all instance variables
+    /**
+     * All-argument constructor
+     * Initialize all information about the general practitioner (including basic information inherited from the parent class and the location of its own clinic)
+     */
     public GeneralPractitioner(int id, String name, String qualification, String clinicLocation) {
         super(id, name, qualification);
         this.clinicLocation = clinicLocation;
     }
 
-    // Method to print all details including type
+    /**
+     * Override the parent method to print the full information for the general practitioner
+     * Call the parent printDetails() class to print the basic information and then fill in the clinic location
+     */
     @Override
     public void printDetails() {
         System.out.println("The health professional details are (General Practitioner):");
@@ -26,4 +35,5 @@ public class GeneralPractitioner extends HealthProfessional {
         System.out.println("Clinic Location: " + clinicLocation);
         System.out.println();
     }
+
 }
